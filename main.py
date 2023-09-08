9a58ed33e643cd5eee62bf106ce6a8e6c7a2fc01
alimandev <aliman@ptolemay.com>
2023-09-08 21:03:24 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+import os
+import requests
+
+from typing import List
+
+from scrape import InstagramParser
+
+
+def get_or_create_path(profile_name):
+    dir_path = f"posts/{profile_name}"
+    if not os.path.exists(dir_path):
+        os.makedirs(dir_path)
+        print(f"Каталог {dir_path} создан успешно.")
+    else:
+        print(f"Каталог {dir_path} уже существует.")
+
+    return dir_path
+
+
+def images_save(images_url: List[str], save_directory: str, post_number: int):
+    for photo_number, image_url in enumerate(images_url):
+        filename = os.path.join(save_directory, f'post_{post_number}_photo_{photo_number + 1}.png')
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open(filename, 'wb') as file:
+                file.write(response.content)
+            print(f"Изображение сохранено в {filename}")
+        else:
+            print(f"Не удалось скачать изображение. Код состояния: {response.status_code}, {response.reason}")
+
+
+if __name__ == '__main__':
+    f = open(f'profiles.txt', 'rb')
+    profiles = f.readlines()
+    f.close()
+    for profile in profiles:
+        profile_name = profile.decode().replace('\n', '')
+        directory_path = get_or_create_path(profile_name)
+
+        iterator = InstagramParser(username=profile_name)
+        i_count = 1
+        while True:
+            try:
+                data = next(iterator)
+                images = data['images']
+                images_save(images, directory_path, i_count)
+                i_count += 1
+            except Exception as e:
+                print(f'Raise error message: {e}')
+                break
+
---
+++ scrape.py
@@ -0,0 +1,55 @@
+from typing import List, Dict
+
+from instaloader import Post, Profile, Instaloader
+
+
+class InstagramParser:
+    def __init__(self, username: str):
+        self.client: Profile = self._load_client(username)
+        self.post_iterator = self.client.get_posts()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self) -> Dict[str, str]:
+        try:
+            post = next(self.post_iterator)
+            if post.typename in ['GraphImage', 'GraphSidecar']:
+                return self._reformat_post(post)
+            else:
+                return self.__next__()
+        except StopIteration:
+            raise StopIteration
+
+    def get_posts_as_list(self) -> List[Dict]:
+
+        data = []
+        for post in self.post_iterator:
+            if post.typename in ['GraphImage', 'GraphSidecar']:
+                reformat_post = self._reformat_post(post)
+                print('post', reformat_post)
+                data.append(reformat_post)
+        return data
+
+    def _reformat_post(self, post: Post) -> Dict[str, str]:
+        return {
+            'title': post.title,
+            'caption': post.caption,
+            'caption_hashtags': post.caption_hashtags,
+            'media_id': post.mediaid,
+            'owner_id': post.owner_id,
+            'date_utc': post.date_utc,
+            'typename': post.typename,
+            'media_count': post.mediacount,
+            'images': self._get_node_images(post) if post.typename == 'GraphSidecar' else [post.url]
+        }
+
+    @staticmethod
+    def _get_node_images(post) -> List[str]:
+        return [i.display_url for i in post.get_sidecar_nodes()]
+
+    @staticmethod
+    def _load_client(username: str) -> Profile:
+        loader = Instaloader()
+        client = Profile.from_username(loader.context, username)
+        return client
